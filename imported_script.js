70a4edda673cff1e303d494ebd13bd5e59574bf3
Matej Topolovac <>
2023-10-02 10:56:22 +0200

    feature: added importScript example

---
+++ imported_script.js
@@ -0,0 +1,3 @@
+console.log("Script imported successfully from webworker with importScript!");
+
+const example_var = "example_var";
--- worker.js
+++ worker.js
@@ -1,3 +1,5 @@
+importScripts("https://cdn.jsdelivr.net/npm/web_worker_npm_module@latest/imported_script.js");
+
 self.onmessage = function (event) {
   console.log("worker.js: onmessage", event);
   const data = event.data;
