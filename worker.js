92c2400b5b7257b90a784dfba7e503f31530c91a
Matej Topolovac <>
2023-10-02 11:07:30 +0200

    chore: added example var from imported script to worker response

--- worker.js
+++ worker.js
@@ -1,8 +1,11 @@
-importScripts("https://cdn.jsdelivr.net/npm/web_worker_npm_module@latest/imported_script.js");
+importScripts(
+  "https://cdn.jsdelivr.net/npm/web_worker_npm_module@latest/imported_script.js"
+);
 
 self.onmessage = function (event) {
   console.log("worker.js: onmessage", event);
   const data = event.data;
   const result = data;
+  result.example_var = example_var;
   self.postMessage(result);
 };
